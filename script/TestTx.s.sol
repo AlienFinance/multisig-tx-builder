aaceaa3eddf093c384564ef7dff0f9c5eb43b55f
void <void@alien.finance>
2024-03-14 22:03:17 +0800

    Test script

---
+++ script/TestTx.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import { BatchScript } from "forge-safe/BatchScript.sol";
+import { IERC20 } from "forge-std/interfaces/IERC20.sol";
+
+contract ExampleScript is BatchScript {
+
+    address multisig = 0x7a66F664FA2B8453C4df1F0243821cd1D4625d03;
+
+    function run(bool send) external isBatch(multisig) {
+        addToBatch(multisig, new bytes(0));
+
+        vm.startBroadcast();
+
+        executeBatch(send, 4);
+
+        vm.stopBroadcast();
+    }
+}
